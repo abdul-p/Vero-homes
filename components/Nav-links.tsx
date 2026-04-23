1072b3a8404a5c660835621e6aaa598a37210e39
abdul-p <abdulahisholabi@gmail.com>
2026-04-23 16:32:16 +0100

    prop error fix

--- components/Nav-links.tsx
+++ components/Nav-links.tsx
@@ -1,33 +1,37 @@
 import Link from "next/link";
 
-export default function NavLinks(setMenuOpen?: (open: boolean) => void) {
+type NavLinksProps = {
+  setMenuOpen?: (open: boolean) => void;
+};
+
+export default function NavLinks({ setMenuOpen }: NavLinksProps) {
   return (
     <div className="flex items-center gap-6">
       <Link
         href="/listings"
         className="text-sm font-semibold text-gray-600 hover:text-lime-600 transition"
-        onClick={() => setMenuOpen(false)}
+        onClick={() => setMenuOpen?.(false)}
       >
         Listings
       </Link>
       <Link
         href="/buy"
         className="text-sm font-semibold text-gray-600 hover:text-lime-600 transition"
-        onClick={() => setMenuOpen(false)}
+        onClick={() => setMenuOpen?.(false)}
       >
         Buy
       </Link>
       <Link
         href="/rent"
         className="text-sm font-semibold text-gray-600 hover:text-lime-600 transition"
-        onClick={() => setMenuOpen(false)}
+        onClick={() => setMenuOpen?.(false)}
       >
         Rent
       </Link>
       <Link
         href="/about"
         className="text-sm font-semibold text-gray-600 hover:text-lime-600 transition"
-        onClick={() => setMenuOpen(false)}
+        onClick={() => setMenuOpen?.(false)}
       >
         About
       </Link>
